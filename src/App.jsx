92d80ae95fce0b6a57ef204fe68a2f039740cea1
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 14:18:24 +0200

    set up for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-const employeesUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
+const employeesUrl = "https://edwardtanguay.vercel.app/share/employees.json";
+
+const employees = [];
 
 function App() {
-
-  useEffect(() => {
-    console.log('nnn');
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(employeesUrl);
+			const employees = await response.json();
+			console.log(employees);
+			console.log(`there are ${employees.length} employees`);
+		})();
+	}, []);
 
 	return (
 		<main>
 			<h1 className="text-3xl text-orange-500 italic">Employee Site</h1>
+			<p>There are {employees.length} employees.</p>
 		</main>
 	);
 }
