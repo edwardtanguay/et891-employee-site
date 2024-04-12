a54f4647151c37476b6bb04da0fadf44a8781904
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 14:36:47 +0200

    showing emps

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import { useEffect, useState } from "react";
 const employeesUrl = "https://edwardtanguay.vercel.app/share/employees.json";
 
 function App() {
-  const [employees, setEmployees] = useState([]);
+	const [employees, setEmployees] = useState([]);
 
 	useEffect(() => {
-		(async () => {
+		setTimeout(async () => {
 			const response = await fetch(employeesUrl);
 			const _employees = await response.json();
-      setEmployees(_employees); // start saving employees to the state variable
-		})();
+			setEmployees(_employees); // start saving employees to the state variable
+		}, 1000);
 	}, []);
 
 	return (
 		<main>
 			<h1 className="text-3xl text-orange-500 italic">Employee Site</h1>
 			<p>There are {employees.length} employees.</p>
+      <ul>
+        {employees.map(employee => {
+          return (
+            <li key={employee.employeeID}>{employee.firstName} {employee.lastName}</li>
+          )
+        })}
+      </ul>
 		</main>
 	);
 }
