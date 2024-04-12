c99e3af1c60ab7252cd2d26f2c942efc80308b03
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 15:38:22 +0200

    basic image in

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -2,7 +2,8 @@
 
 export const Employee = ({employee}) => {
 	return (
-		<div className="bg-slate-500 mb-3 p-3 rounded" key={employee.employeeID}>
+		<div className="bg-slate-500 mb-3 p-3 rounded w-full md:w-[30rem]" key={employee.employeeID}>
+			<img className="w-[5rem] rounded" src={`images/employees/employee_${employee.employeeID}.jpg`}/>
 			{employee.firstName} {employee.lastName}
 		</div>
 	);
