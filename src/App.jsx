a1672dd473719722696e24987462bb27c3deba53
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 14:00:44 +0200

    useEffect works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+
+const employeesUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
+
 function App() {
+
+  useEffect(() => {
+    console.log('nnn');
+  }, []);
+
 	return (
 		<main>
 			<h1 className="text-3xl text-orange-500 italic">Employee Site</h1>
