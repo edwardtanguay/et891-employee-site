089a8ce26b76d480c39ef32bce563c89fd603acb
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 13:53:57 +0200

    styled in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <main>
-      <h1>Employee Site</h1>
-    </main>
-  )
+	return (
+		<main>
+			<h1>Employee Site</h1>
+		</main>
+	);
 }
 
-export default App
+export default App;
