8d9ee53ef7fc02eee1623337d50dee41ebe1b795
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 15:40:06 +0200

    images in

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable react/prop-types */
 
-export const Employee = ({employee}) => {
+export const Employee = ({ employee }) => {
 	return (
-		<div className="bg-slate-500 mb-3 p-3 rounded w-full md:w-[30rem]" key={employee.employeeID}>
-			<img className="w-[5rem] rounded" src={`images/employees/employee_${employee.employeeID}.jpg`}/>
-			{employee.firstName} {employee.lastName}
+		<div
+			className="bg-slate-500 mb-3 p-3 rounded w-full md:w-[30rem] flex gap-2"
+			key={employee.employeeID}
+		>
+			<img
+				className="w-[5rem] rounded"
+				src={`images/employees/employee_${employee.employeeID}.jpg`}
+			/>
+			<div>
+				<div className="font-semibold">
+					{employee.firstName} {employee.lastName}
+				</div>
+				<div className="italic">{employee.title}</div>
+			</div>
 		</div>
 	);
 };
