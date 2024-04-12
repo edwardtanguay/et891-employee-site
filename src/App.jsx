fecd9c14340222c1b2d1e366d3cb3dd0bdb3ba87
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-12 16:02:28 +0200

    filterered employees ready

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import { Employee } from "./components/Employee";
 const employeesUrl = "https://edwardtanguay.vercel.app/share/employees.json";
 
 function App() {
-	// const [originalEmployees, setOriginalEmployees] = useState([]);
-	// const [filteredEmployees, setFilteredEmployees] = useState([]);
-	const [employees, setEmployees] = useState([]);
+	const [originalEmployees, setOriginalEmployees] = useState([]);
+	const [filteredEmployees, setFilteredEmployees] = useState([]);
+	// const [employees, setEmployees] = useState([]);
 	const [searchText, setSearchText] = useState('');
 
 	useEffect(() => {
 		setTimeout(async () => {
 			const response = await fetch(employeesUrl);
 			const _employees = await response.json();
-			setEmployees(_employees); // start saving employees to the state variable
+			setOriginalEmployees(_employees); 
+			setFilteredEmployees(_employees); 
 		}, 0);
 	}, []);
 
@@ -28,17 +29,17 @@ function App() {
 		<main>
 			<Header />
 			<input value={searchText} onChange={(e) => handleSearchTextChange(e)} className="w-full text-3xl rounded mb-3 md:w-[30rem]" placeholder="search employees"/>
-			{employees.length === 0 ? (
+			{filteredEmployees.length === 0 ? (
 				<p>
 					<ImSpinner9 className="animate-spin text-3xl text-yellow-300" />
 				</p>
 			) : (
 				<>
 					<p className="mb-3">
-						There are {employees.length} employees.
+						There are {filteredEmployees.length} employees.
 					</p>
 					<div>
-						{employees.map((employee) => {
+						{filteredEmployees.map((employee) => {
 							return (
 								<Employee
 									employee={employee}
